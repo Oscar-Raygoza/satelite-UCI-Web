1fb5ee9072efd4ebda8ede383c8ae038e39e29cc
OscarRaygoza <oscar.eduardo.raygoza@gmail.com>
2019-03-06 22:46:54 -0600

    Actualizacion del dashboard

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 const sockets = io();
 
+
+
+/*    MAPA    */
 var map;
 function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
@@ -8,9 +11,7 @@ function initMap() {
   });
 }
 
-/**/
-
-var mymap = L.map('mapid').setView([21.857125, -102.289837], 20);
+var mymap = L.map('mapid').setView([21.857125, -102.289837], 18);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
@@ -20,10 +21,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   id: 'mapbox.streets'
 }).addTo(mymap);
 
-/**/ 
 var gps = L.marker([21.857125, -102.289837]);
 mymap.addLayer(gps);
-/**/ 
+
+/* FIN FUNCION MAPA*/ 
+
+/* VARIABLES LOCALES */
 var now = new Date();  
 let time = now.getDate();
 
@@ -33,29 +36,48 @@ var dataGraphTemp = [],
 
 let i=0;
 
+/**FIN VARIABLES LOCALES  */
+
+
+
 sockets.on('data', function(data){
+  /**INICIO DE DATA CON SOCKETS */
   var dataJSON = JSON.parse(data);
-  
+  console.log(dataJSON);
   i++;
+/**FIN DATA SOCKETS */
+
+
+  /**DATOS DE TEMPERATURA SOKETS */
   dataGraphTemp.push({x: i, Cen: dataJSON.Sensores[0][5], Far: ((9/5)*dataJSON.Sensores[0][5]+32)});
   ChartTemperature.setData(dataGraphTemp);
-  
+  /**FIN DE DATOS TEMPERATURA */
+
+  /**DATOS DE HUMEDAD SOKETS */
   dataGraphHum.push({x: i, Hum: dataJSON.Sensores[0][3]});
   ChartHum.setData(dataGraphHum);
+  /**FIN DE DATOS HUMEDAD */
   
-  console.log(dataJSON);
+  
+  
+
+  /* MAPA SOCKET */
   var pos = {
     lat:  dataJSON.Sensores[0][15],
     long: dataJSON.Sensores[0][16]
   }
+
   gps.setLatLng([dataJSON.Sensores[0][15],dataJSON.Sensores[0][16]]).update();
-  //posizioni.push(pos);
+  posizioni.push(pos);
   //var polyline = L.polyline([dataJSON.Sensores[0][15],dataJSON.Sensores[0][16]], {color: 'red'}).addTo(mymap);
 	//mymap.fitBounds(polyline.getBounds());
-  gps.bindPopup( "CatSat").openPopup();
-})
+  gps.bindPopup("SateliteUCI").openPopup();
+/**FIN DE MAPA SOCKET */
 
+});
+/**/
 
+/* TEMPERATURA SENSOR */
 var ChartTemperature = new Morris.Area({
   element: 'chartTemperature',
   xkey: 'x',
@@ -65,6 +87,10 @@ var ChartTemperature = new Morris.Area({
   lineColors: ['yellow', 'red']
 });
 
+/* FIN TEMPERATURA SENSOR */
+
+
+/* HUMEDAD SENSOR */ 
 var ChartHum = new Morris.Area({
   element: 'chartHum',
   xkey: 'x',
@@ -73,3 +99,4 @@ var ChartHum = new Morris.Area({
   resize: false,
   lineColors: ['#44C0FF']
 });
+/* FIN SENSOR  */
